ecd471540246057cc4bb13a93bb7595e60e3f57e
Renato1909 <renato.aparecido.business@gmail.com>
2025-05-24 16:29:54 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.operacoestpa.tabuada;
+import java.util.Scanner;
+/**
+ *
+ * @author RENATO
+ */
+public class Tabuada {
+
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número para ver a tabuada:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
